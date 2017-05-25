8499e60fdda0248776cf2b7e2196c59b9d9d5536
ninsy <kwojciechowski094@gmail.com>
2017-05-25 23:05:16 +0200

    webpack fakap

--- webpack.config.js
+++ webpack.config.js
@@ -12,20 +12,23 @@ module.exports = {
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.IgnorePlugin(/\.(md|json)?$/),
         new webpack.BannerPlugin('require("source-map-support").install();',
-            { raw: true, entryOnly: false }),
+            {raw: true, entryOnly: false}),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('dev')
         })
     ],
     module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ["es2015"]
-            }
-        }]
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ["es2015"]
+                },
+            },
+            {include: /\.json$/, loaders: ["json-loader"]},
+        ],
     },
     target: 'node',
     devtool: 'sourcemap',
